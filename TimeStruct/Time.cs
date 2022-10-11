62e7b2052dc49212db179477b3bbe5d26427c320
Watсher <Wather@DESKTOP-VL24RQ1>
2022-10-11 19:11:56 +0300

    All problems/mistakes are fixed.

--- TimeStruct/Time.cs
+++ TimeStruct/Time.cs
@@ -7,7 +7,7 @@ namespace TimeStruct
     /// <summary>
     /// Struct Time represents the times in 24-hours format without date.
     /// </summary>
-    public struct Time
+    public readonly struct Time
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="Time"/> struct.
@@ -25,45 +25,30 @@ namespace TimeStruct
         /// </summary>
         /// <param name="hours" > Number of horse, can be any integer. </param>
         /// <param name="minutes"> Number of minuts, can be any integer. </param>
-        public Time(int hours, int minutes)
-        {
-            this.Hours = (hours + (minutes / 60)) % 24;
-            this.Minutes = minutes % 60;
-
-            if (this.Minutes < 0)
-            {
-                this.Hours -= 1;
-                this.Minutes = 60 + this.Minutes;
-            }
-
-            if (this.Hours < 0)
-            {
-                this.Hours = 24 + this.Hours;
-            }
-        }
+        public Time(int hours, int minutes) => (this.Hours, this.Minutes) = TimeConvert(hours, minutes);
 
         /// <summary>
         /// Gets property.
         /// </summary>
-        public readonly int Hours { get; }
+        public int Hours { get; }
 
         /// <summary>
         /// Gets property.
         /// </summary>
-        public readonly int Minutes { get; }
+        public int Minutes { get; }
 
         /// <summary>
         /// Gives representation of given time in string format "Minuts : Horse.
         /// </summary>
         /// <returns> String representation of the Time. </returns>
-        public string ToString()
+        public new string ToString()
         {
-            StringBuilder result = new StringBuilder();
-            result.Append(DigitToString(this.Hours / 10));
-            result.Append(DigitToString(this.Hours % 10));
+            var result = new StringBuilder();
+            result.Append(this.Hours / 10);
+            result.Append(this.Hours % 10);
             result.Append(':');
-            result.Append(DigitToString(this.Minutes / 10));
-            result.Append(DigitToString(this.Minutes % 10));
+            result.Append(this.Minutes / 10);
+            result.Append(this.Minutes % 10);
             return result.ToString();
         }
 
@@ -72,25 +57,25 @@ namespace TimeStruct
         /// </summary>
         /// <param name="hours"> Given hours. </param>
         /// <param name="minutes"> Given minutes. </param>
-        public void Deconstruct(out int hours, out int minutes)
-        {
-            minutes = this.Minutes;
-            hours = this.Hours;
-        }
+        public void Deconstruct(out int hours, out int minutes) => (minutes, hours) = (this.Minutes, this.Hours);
 
-        private static char DigitToString(int number) => number switch
+        private static (int, int) TimeConvert(int hours, int minutes)
         {
-            0 => '0',
-            1 => '1',
-            2 => '2',
-            3 => '3',
-            4 => '4',
-            5 => '5',
-            6 => '6',
-            7 => '7',
-            8 => '8',
-            9 => '9',
-            _ => ':',
-        };
+            int resHours = (hours + (minutes / 60)) % 24;
+            int resMinutes = minutes % 60;
+
+            if (resMinutes < 0)
+            {
+                resHours -= 1;
+                resMinutes = 60 + resMinutes;
+            }
+
+            if (resHours < 0)
+            {
+                resHours = 24 + resHours;
+            }
+
+            return (resHours, resMinutes);
+        }
     }
 }
